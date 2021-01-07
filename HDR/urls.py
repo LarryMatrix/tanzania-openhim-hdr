94a46aa082edd9e7c72670e66fadd8ee4a49e034
Danford Nchembi <danfordnchembi@gmail.com>
2021-01-07 08:24:41 +0300

    codacy score improvements

--- HDR/urls.py
+++ HDR/urls.py
@@ -1,5 +1,6 @@
 
-"""HDR URL Configuration
+"""
+HDR URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
